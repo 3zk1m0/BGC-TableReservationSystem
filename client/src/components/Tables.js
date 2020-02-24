9295f5a94a63f663e5ea1449b31a194fbc57cfa3
Thomas Hillebrink <thomas.hillebrink@digitalentshelsinki.fi>
2020-02-24 10:33:43 +0200

    finished restructuring react files

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -1,8 +1,101 @@
 import React from 'react'
 
+const MOCK_DATABASE = {
+    "tables": {
+        "table1": {
+            "capacity": 2,
+            "min_players": 1
+        },
+        "table2": {
+            "capacity": 3,
+            "min_players": 2
+        },
+        "table3": {
+            "capacity": 7,
+            "min_players": 4
+        },
+        "table4": {
+            "capacity": 6,
+            "min_players": 3
+        },
+        "table5": {
+            "capacity": 6,
+            "min_players": 3
+        },
+        "table6": {
+            "capacity": 7,
+            "min_players": 4
+        },
+        "table7": {
+            "capacity": 10,
+            "min_players": 5
+        },
+        "table8": {
+            "capacity": 4,
+            "min_players": 2
+        },
+        "table9": {
+            "capacity": 4,
+            "min_players": 2
+        }
+    }
+}
+const Tables = ({newReservation, setnewReservation}) => {
 
-const Tables = () => {
-
+    const Table = ({tableNumber}) => {
+        let elementId = `table${tableNumber}`
+        return (
+            <div id={elementId} className="table">{tableNumber}</div>
+        )
+    }
+    
+    const Tables = () => {
+        return (
+            <div id="tables">
+                <RelevantTables />
+            </div>
+        )
+    }
+    
+    const RelevantTables = () => {
+        /*const [clicks, setClicks] = useState({
+            left: 0, right: 0
+        })*/
+    
+        let tables = getRelevantTableIds()
+        
+        return tables.map(table => {
+            return <Table tableNumber={table} />
+        })
+    }
+    
+    let getPlayerCount = () => {
+        /*
+        Return the amount of players expected as well as the amount of strangers allowed.
+        */
+        return newReservation.heads + newReservation.strangers
+    }
+    
+    let getRelevantTableIds = () => {
+        /*
+        Return a list of table IDs which are appropriate for the selected player count.
+        */
+        let tableIds = []
+        let players = getPlayerCount()
+    
+        for (let tableId in MOCK_DATABASE.tables) {
+            if (!MOCK_DATABASE.tables.hasOwnProperty(tableId)) {
+                continue
+            }
+            let table = MOCK_DATABASE.tables[tableId]
+            console.log(`Players: ${players}, ID: ${tableId}, Capacity: ${table.capacity}, Min Players: ${table.min_players}`)
+            if (players <= table.capacity && players >= table.min_players) {
+                tableIds.push(tableId.slice(-1))
+            }
+        }
+    
+        return tableIds
+    }
     return (
         <div>
             <h5>fancy image of tables</h5>
--- client/src/index.js
+++ client/src/index.js
@@ -12,28 +12,29 @@ const App = () => {
   const now = new Date()
   const currentDate = now.getDate()
 
-  const minutes = (((now.getMinutes() + 7.5)/15 | 0) * 15) % 60;
-  const time = now.getHours() + ":" + minutes
+  const minutes = (((now.getMinutes() + 7.5)/15 | 0) * 15) % 60; //Round off minutes to 15 minute intervals
+  now.setMinutes(minutes)
   
   const [allReseverations, setallReseverations] = useState([])
-  const [newReservation, setNewReservation] = useState({ Date: currentDate, Time: time, Name: undefined, Heads: 0, Strangers: 0, Table: undefined, Duration: 0}) //this is pushed into MongoBD when customer finishes their reservation
+  const [newReservation, setNewReservation] = useState({ startTime: now, endTime: undefined, Name: undefined, Heads: 0, Strangers: 0, Table: undefined, Duration: 0, Arrived: false}) //this is pushed into MongoBD when customer finishes their reservation
   console.log(newReservation);
   
   return (
     <>
      <Heads 
-      newReservation={newReservation}
-      setNewReservation={setNewReservation}
+        newReservation={newReservation}
+        setNewReservation={setNewReservation}
      />
      <Strangers 
-      newReservation={newReservation}
-      setNewReservation={setNewReservation}
+        newReservation={newReservation}
+        setNewReservation={setNewReservation}
      />
      <Time 
         newReservation={newReservation}
         setNewReservation={setNewReservation}
      />
      <Tables 
+        allReseverations={allReseverations}
         newReservation={newReservation}
         setNewReservation={setNewReservation}
      />
