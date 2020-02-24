81d4d601b3958bc1a32b1c5f85150208d8b4b625
Thomas Hillebrink <thomas.hillebrink@digitalentshelsinki.fi>
2020-02-24 15:31:35 +0200

    Date can now be selected

--- client/src/components/Time.js
+++ client/src/components/Time.js
@@ -1,24 +1,67 @@
 import React, { useState } from 'react'
 
 
-const Time = (newReservation, setNewReservation) => {
+const Time = ({newReservation, setNewReservation}) => {
+  console.log(setNewReservation);
+  
 
+const handleDataSelection = (event) => {
+
+    const date = new Date(event.target.value)
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const day = date.getDate()
+
+    const hour = newReservation.startTime.getHours()
+    const minutes = newReservation.startTime.getMinutes()
+
+    const newDate = new Date(year, month, day, hour, minutes)
+  
+    setNewReservation({...newReservation, startTime: newDate})
+  
+  }
 
 const handleAddDuration = () => {
-  const newDuration = newReservation.Duration + 15
-  setNewReservation({...newReservation, Duration: newDuration})
+
+
 }
+
 const handleRemoveDuration = () => {
   const newDuration = newReservation.Duration - 15
   setNewReservation({...newReservation, Duration: newDuration})
 }
 
-  let onTimeBackwardPress = () => {
-    changeTime(-15)
+let onTimeBackwardPress = () => {
+
+  const year = newReservation.startTime.getFullYear()
+  const month = newReservation.startTime.getMonth()
+  const day = newReservation.startTime.getDay()
+
+  const hour = newReservation.startTime.getHours()
+  const minutes = newReservation.startTime.getMinutes() - 15
+
+  const newDate = new Date(year, month, day, hour, minutes)
+
+  setNewReservation({...newReservation, startTime: newDate})
+  console.log(newReservation)
 }
 
 let onTimeForwardPress = () => {
-    changeTime(15)
+
+  const year = newReservation.startTime.getFullYear()
+  const month = newReservation.startTime.getMonth()
+  const day = newReservation.startTime.getDay()
+  const hour = newReservation.startTime.getHours()
+
+  const newMinutes = newReservation.startTime.getMinutes() + (15 * 60 * 1000)
+  console.log(newMinutes)
+  const minutes = newReservation.startTime.setMinutes(newMinutes)
+  console.log(minutes)
+
+  const newDate = new Date(year, month, day, hour, minutes)
+
+  setNewReservation({...newReservation, startTime: newDate})
+  console.log(newReservation)
 }
 
 let getTime = () => {
@@ -41,10 +84,10 @@ const changeTime = (minutes) => {
 
     return (  
       <p>
-        <input type="date"></input>
+        <input type="date" onChange={handleDataSelection}></input>
         Start time:
         <button type="button" onClick={onTimeBackwardPress}>&lt;</button>
-        <input type="text" id="time" value={newReservation.Time} readOnly></input>
+        <input type="text" id="time" value={`${newReservation.startTime.getHours()}:${!newReservation.startTime.getMinutes() ? "00" : newReservation.startTime.getMinutes()}`} readOnly></input>
         <button type="button" onClick={onTimeForwardPress}>&gt;</button>
               
         Duration: <input type="text" id="duration" value={newReservation.Duration} onInput={null}></input>
